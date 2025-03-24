6a714adbbb0484be054ce3d2b8d2a8a011a7dbaf
Bogdan Yarovenko <yarovenkobogdan66@gmail.com>
2025-03-24 13:27:20 +0100

    Version qui work

---
+++ IAcademyOfDoom/View/DifficultySelect.cs
@@ -0,0 +1,30 @@
+﻿using IAcademyOfDoom.Logic.GameSettings;
+using System;
+using System.Windows.Forms;
+
+namespace IAcademyOfDoom.View
+{
+    public partial class DifficultySelect : Form
+    {
+        public Difficulty? Difficulty { get { return difficultyListBox.Items[difficultyListBox.SelectedIndex == -1 ? 0 : difficultyListBox.SelectedIndex ] as Difficulty?; } }
+        public string InputName { get { return nameTextBox.Text; } }
+        public DifficultySelect()
+        {
+            InitializeComponent();
+            foreach (Difficulty difficulty in Enum.GetValues(typeof(Difficulty)))
+            {
+                difficultyListBox.Items.Add(difficulty);
+            }
+        }
+
+        private void DifficultySelect_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void okButton_Click(object sender, EventArgs e)
+        {
+            Dispose();
+        }
+    }
+}
