4dc8764ce08ed6ffc4c8a0ea363a197df5be471c
Timéo Meunier <timecraftyt33@gmail.com>
2025-04-13 20:46:11 +0200

    Ajout de la methode pour la salle Rest Room

---
+++ IAcademyOfDoom/Logic/Places/RoomLounge.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IAcademyOfDoom.App;
+using IAcademyOfDoom.Logic.Mobiles;
+
+namespace IAcademyOfDoom.Logic.Places
+{
+    internal class RoomLounge : Room
+    {
+        public RoomLounge(int x, int y) : base(x, y)
+        {
+            Type = RoomType.Facility; 
+            Name = "Lounge Room";
+        }
+
+        public override object ActOnEntry(Botling botling)
+        {
+
+            // TO:DO
+            return null;
+        }
+    }
+}
---
+++ IAcademyOfDoom/Logic/Places/RoomRest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IAcademyOfDoom.Logic.GameSettings;
+using IAcademyOfDoom.Logic.Mobiles;
+
+
+namespace IAcademyOfDoom.Logic.Places
+{
+    internal class RoomRest : Room
+    {
+        public RoomRest(int x, int y) : base(x, y)
+        {
+            Type = RoomType.Facility;
+            Name = "Rest Room";
+        }
+
+        public override object ActOnEntry(Botling botling)
+        {
+            int hpInit = Default.BaseHitPoints(Game.Difficulty);
+            int hpLost = hpInit - botling.HP;
+
+            if (hpLost > 0) {
+
+                
+                botling.HP += hpLost / 2;
+
+
+            }
+            return null;
+        }
+    }
+}
